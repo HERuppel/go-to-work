73d146ff752734a0f6aca0273701d2700673f0cf
Renato Ruppel <heruppel2@gmail.com>
2024-11-30 12:47:08 -0300

    fix(models): add city to address struct

--- internal/models/Address.go
+++ internal/models/Address.go
@@ -4,6 +4,7 @@ type Address struct {
 	ID         uint64 `json:"id"`
 	Country    string `json:"country"`
 	Uf         string `json:"uf"`
+	City       string `json:"city"`
 	Street     string `json:"street"`
 	Zipcode    string `json:"zipcode"`
 	District   string `json:"district,omitempty"`
